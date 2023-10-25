25ba1e5327360507a31a09ed14c2b2a148f0fa3c
Wildric <wildric.auric@gmail.com>
2023-10-25 10:36:35 +0200

    Added input component's binding

--- src/space-shooter/ecs/systems/input_system.cpp
+++ src/space-shooter/ecs/systems/input_system.cpp
@@ -13,20 +13,24 @@
 
 namespace space_shooter::ecs {
 
-InputSystem::InputSystem() : System{type_list<InputComponent>{}} {}
+    InputSystem::InputSystem() : System{ type_list<InputComponent>{} } {}
 
-void InputSystem::update(const sf::Time &delta_time,
-                         std::vector<Entity *> &entities, Manager &manager) {
+    void InputSystem::update(const sf::Time& delta_time, std::vector<Entity*>& entities, Manager& manager) {
 
-  for (auto e : entities) {
-    assert(hasRequiredComponents(*e));
 
-    // auto &input = e->get<InputComponent>();
-
-    // TODO : reset detected input from keyboard (put all value of component to
-    // false)
-    // TODO: detect keypressed with SFML and set the boolean state accordingly
-  }
-}
+        for (auto e : entities) {
+            assert(hasRequiredComponents(*e));
+            space_shooter::ecs::InputComponent& input = e->get<InputComponent>();
+            input.escape = sf::Keyboard::isKeyPressed(sf::Keyboard::Escape);
+            input.enter = sf::Keyboard::isKeyPressed(sf::Keyboard::Enter);
+            input.move_bottom = sf::Keyboard::isKeyPressed(sf::Keyboard::Down);
+            input.move_right = sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
+            input.move_top = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);
+            input.move_left = sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
+            // TODO : reset detected input from keyboard (put all value of component to
+            // false)
+            // TODO: detect keypressed with SFML and set the boolean state accordingly
+        }
+    }
 
 } // namespace space_shooter::ecs
