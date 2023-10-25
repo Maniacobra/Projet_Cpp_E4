63cb007e2b478dbc2f042fe18fa565933e6a2ff9
Wildric <wildric.auric@gmail.com>
2023-10-25 12:13:51 +0200

    Added texture loading

--- src/space-shooter/ecs/systems/rendering_system.cpp
+++ src/space-shooter/ecs/systems/rendering_system.cpp
@@ -22,14 +22,22 @@ void RenderingSystem::update(const sf::Time & /*delta_time*/,
                              Manager &manager) {
   for (auto e : entities) {
     assert(hasRequiredComponents(*e));
-    std::cout << "Ship update." << std::endl;
-    // const auto &pos = e->get<PositionComponent>();
-    // const auto &spr = e->get<SpriteComponent>();
-    // auto &tex = e->get<TextureComponent>();
+    const auto &pos = e->get<PositionComponent>();
+    const auto &spr = e->get<SpriteComponent>();
+    auto &tex = e->get<TextureComponent>();
 
-    // TODO load texture (if needed) into the texture component
-    // TODO then set boolean of component to true
-
+    // DONE: load texture (if needed) into the texture component
+    // DONE: then set boolean of component to true
+    if (!tex.loaded && tex.texture.loadFromFile(tex.texture_path.string())) 
+        tex.loaded = 1;
+   
+  
+    sf::Sprite sfml_sprite;
+    sfml_sprite.setTexture(tex.texture);
+    sfml_sprite.setPosition(pos.x, pos.y);
+    
+    manager.gameState().rendering_window->draw(sfml_sprite);
+    
     // TODO build SFML sprite to render the texture
     // TODO render the sprite at the correct position via manager -> game state
     // -> rendering window -> draw
