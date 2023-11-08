3ea6cec7eddfc1207c3098bcec83da20b3864286
Wildric <wildric.auric@gmail.com>
2023-11-08 11:56:18 +0100

    fixed local path issue

--- include/space-shooter/ecs/entities/player_ship.hpp
+++ include/space-shooter/ecs/entities/player_ship.hpp
@@ -12,8 +12,7 @@ namespace space_shooter::ecs {
 
 class PlayerShipEntity : public Entity {
 public:
-  PlayerShipEntity(sf::Vector2f pos, const std::filesystem::path &texture_path,
-                   sf::Vector2f velocity);
+  PlayerShipEntity(sf::Vector2f pos, const std::filesystem::path &texture_path, sf::Vector2f velocity);
   PlayerShipEntity() = delete;
 
   using Entity::Entity;
--- src/space-shooter/scenes/level.cpp
+++ src/space-shooter/scenes/level.cpp
@@ -13,13 +13,11 @@ namespace space_shooter {
 	void initLevel(ecs::Manager &manager) {
   
 		std::cout << "Init level." << std::endl;
-		//TODO: Make see make set direcltly project path to look at 
-		std::filesystem::path player_path = "..\\..\\..\\assets\\textures\\player.png";
-		std::filesystem::path bg_path     = "..\\..\\..\\assets\\textures\\test.png"; 
-
-		//const std::filesystem::path& texture_path, int window_width, int window_height
-		manager.registerEntity<ecs::PlayerShipEntity>(sf::Vector2f(10.f, 10.f), player_path, sf::Vector2f(1.f, 1.f));
+		static const std::filesystem::path& player_path = manager.gameState().config.path_to_textures / "player.png";
+		static const std::filesystem::path& bg_path     = manager.gameState().config.path_to_textures / "test.png";
+		//Note:Registration order is the same as rendering order
 		manager.registerEntity<ecs::BackgroundEntity>(bg_path, (int)manager.gameState().width, (int)manager.gameState().height);
+		manager.registerEntity<ecs::PlayerShipEntity>(sf::Vector2f(10., 10.f), player_path, sf::Vector2f(1000.f, 1000.f));
 		manager.registerSystem<ecs::RenderingSystem>();
 		manager.registerSystem<ecs::InputSystem>();
 		manager.registerSystem<ecs::MovementSystem>();
