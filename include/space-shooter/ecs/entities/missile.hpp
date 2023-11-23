065af81a15ed4a42d0491bef66de6baec38bab23
Wildric <wildric.auric@gmail.com>
2023-11-23 13:51:02 +0100

    added tag component to missile

--- include/space-shooter/ecs/entities/missile.hpp
+++ include/space-shooter/ecs/entities/missile.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <space-shooter/ecs/entity.hpp>
+#include <space-shooter/ecs/components/tag_component.hpp>
 
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
@@ -12,7 +13,8 @@ namespace space_shooter::ecs {
 
 	class MissileEntity : public Entity {
 	public:
-		MissileEntity(sf::Vector2f pos, sf::Vector2f velocity, int radius, sf::Color color);
+		//tag is enemymissile or playermissile
+		MissileEntity(sf::Vector2f pos, sf::Vector2f velocity, int radius, sf::Color color, EntityTag tag = EntityTag::None);
 		MissileEntity() = delete;
 
 		using Entity::Entity;
--- src/space-shooter/ecs/entites/missile.cpp
+++ src/space-shooter/ecs/entites/missile.cpp
@@ -12,12 +12,14 @@
 
 namespace space_shooter::ecs {
 
-MissileEntity::MissileEntity(sf::Vector2f pos, sf::Vector2f velocity, int radius, sf::Color color) {
+MissileEntity::MissileEntity(sf::Vector2f pos, sf::Vector2f velocity, int radius, sf::Color color, EntityTag tag) {
 
     add<PositionComponent>(pos.x, pos.y);
     add<SpriteComponent>(radius * 2, radius * 2, SpriteComponent::Resize::None, true);
     add<ShapeComponent>(ShapeComponent::ShapeType::Circle, (float)radius, color);
     add<VelocityComponent>(velocity.x, velocity.y);
+    add<TagComponent>(tag);
+
 }
 
 } // namespace space_shooter::ecs
