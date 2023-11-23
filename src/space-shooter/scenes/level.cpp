8670a58600f6f86b6239044ba37f20393b119dd1
Wildric <wildric.auric@gmail.com>
2023-11-23 18:34:06 +0100

    changed background

--- src/space-shooter/scenes/level.cpp
+++ src/space-shooter/scenes/level.cpp
@@ -12,10 +12,10 @@ namespace space_shooter {
 
 		if (!resume) {
 			static const std::filesystem::path& player_path = manager.gameState().config.path_to_textures / "player.png";
-			static const std::filesystem::path& bg_path = manager.gameState().config.path_to_textures / "test.png";
+			static const std::filesystem::path& bg_path = manager.gameState().config.path_to_textures / "bg.png";
 			
 			manager.registerEntity<ecs::BackgroundEntity>(bg_path, (int)manager.gameState().width, (int)manager.gameState().height);
-			manager.registerEntity<ecs::PlayerShipEntity>(sf::Vector2f(10.f, 10.f), 100, 100, player_path, 500.f, 100);
+			manager.registerEntity<ecs::PlayerShipEntity>(sf::Vector2f(10.f, 10.f), 100, 100, player_path, 500.f, 5);
 			manager.registerEntity<ecs::EnemySpawnerEntity>(5000);
 			manager.registerEntity<ecs::MissileEntity>(sf::Vector2f(350.f, 10.f), sf::Vector2f(0.f, 30.f), 10, sf::Color::Blue, ecs::EntityTag::EnemyMissile);
 		}
@@ -38,5 +38,7 @@ namespace space_shooter {
 		manager.registerSystem<ecs::EnemySpawnSystem>();
 		manager.registerSystem<ecs::EnemyShootingSystem>();
 		manager.registerSystem<ecs::EnemyControllerSystem>();
+		
+		manager.registerSystem<ecs::HealthSystem>();
 	}
 }
