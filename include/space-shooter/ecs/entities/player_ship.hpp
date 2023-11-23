b7cb9f7c8272e0596a58684cb97122d642ac5ccb
Maniacobra <maniacobra@orange.fr>
2023-11-23 21:04:26 +0100

    Made enemy move towards player position

--- include/space-shooter/ecs/entities/player_ship.hpp
+++ include/space-shooter/ecs/entities/player_ship.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <space-shooter/ecs/entity.hpp>
+#include <space-shooter/ecs/components/position_component.hpp>
 
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
@@ -11,9 +12,11 @@
 namespace space_shooter::ecs {
 
 class PlayerShipEntity : public Entity {
+  PositionComponent* posCmp;
 public:
   PlayerShipEntity(sf::Vector2f pos, int sizeX, int sizeY, const std::filesystem::path &texture_path, float speed, int health);
   PlayerShipEntity() = delete;
+  PositionComponent* getPos();
 
   using Entity::Entity;
 };
