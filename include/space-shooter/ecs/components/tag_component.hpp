122c797aa564c4fe385fba57da170d4105674a86
Wildric <wildric.auric@gmail.com>
2023-11-23 13:50:03 +0100

    added macro for tag filtering

--- include/space-shooter/ecs/components/tag_component.hpp
+++ include/space-shooter/ecs/components/tag_component.hpp
@@ -2,6 +2,9 @@
 #include <space-shooter/ecs/component.hpp>
 
 
+#define ASSERT_TAG(entity, tag) if (e->get<TagComponent>().type != EntityTag::Enemy) continue;
+
+
 namespace space_shooter::ecs {
 	enum class EntityTag {
 		None,
