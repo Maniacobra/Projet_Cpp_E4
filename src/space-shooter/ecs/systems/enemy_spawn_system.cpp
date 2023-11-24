18103ab2045bab7ac4a03e1e35ad30cfc1bba90d
Wildric <wildric.auric@gmail.com>
2023-11-24 13:12:32 +0100

    improved gameplay, clamped player position to be alwaus below enemies

--- src/space-shooter/ecs/systems/enemy_spawn_system.cpp
+++ src/space-shooter/ecs/systems/enemy_spawn_system.cpp
@@ -9,26 +9,27 @@
 #include <space-shooter/utils.hpp>
 #include <cassert>
 
+
+static int maxNum = 10;
+static int num    = 0;
+
 namespace space_shooter::ecs {
 
     EnemySpawnSystem::EnemySpawnSystem() : System{ type_list<ClockComponent, TagComponent>{} } {}
 
     void EnemySpawnSystem::update(const sf::Time& delta_time, std::vector<Entity*>& entities, Manager& manager) {
+        int currentNum = num;
+        num            = 0;
         for (auto e : entities) {   
             assert(hasRequiredComponents(*e));
+            if (e->get<TagComponent>().type == EntityTag::Enemy) ++num;
             ASSERT_TAG(e, EntityTag::EnemySpawner);
             if (!e->get<ClockComponent>().over) continue;
-            //Tick
-            //Temporary to limit enemies
-            static int maxNum = 10;
-            static int num    = 0;
-            //-----------------------       
-            if (num > maxNum)
+            //limit enemies on screen
+            if (currentNum > maxNum)
                 return;
-
             manager.registerEntity<ecs::EnemyShipEntity>(sf::Vector2f(random(0, manager.gameState().width - 100), 0), 100, 100, 
                  manager.gameState().config.path_to_textures / "ship0.png", 2);
-            ++num;
         }
     }
 
--- src/space-shooter/ecs/systems/health_system.cpp
+++ src/space-shooter/ecs/systems/health_system.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <space-shooter/ecs/systems/health_system.hpp>
-
 #include <space-shooter/ecs/components/all.hpp>
 #include <space-shooter/ecs/entity.hpp>
+#include <space-shooter/ecs/manager.hpp>
+#include <space-shooter/ecs/entities/audio_player_entity.hpp>
 #include <space-shooter/ecs/system.hpp>
 #include <space-shooter/game_state.hpp>
 
@@ -15,15 +16,15 @@ namespace space_shooter::ecs {
     HealthSystem::HealthSystem() : System{ type_list<HealthComponent, ColliderComponent>{} } {}
 
     void HealthSystem::update(const sf::Time& delta_time, std::vector<Entity*>& entities, Manager& manager) {
-
-
         for (auto e : entities) {
             assert(hasRequiredComponents(*e));
             ColliderComponent& col     = e->get<ColliderComponent>();
             HealthComponent&   health  = e->get<HealthComponent>();
             health.health              = fmax(0, health.health - fmin(col.hit, col.maxHitInOneFrame));
-
-            if (health.health == 0)    e->kill();
+            if (health.health == 0) {
+                manager.registerEntity<ecs::AudioPlayerEntity>(manager.gameState().config.path_to_audio / "Death.wav", false); //Not best place to do this
+                e->kill();
+            }
             col.hit                    = 0;
         }
     }
